9fcea5774101e89eed3a477088aa2b7381cd4bf0
Fernando Saraiva <fpsaraiva@gmail.com>
2019-10-30 08:01:22 -0300

    [017] - Desafio Intermediário

--- projeto-loja-fone/js/input-search.js
+++ projeto-loja-fone/js/input-search.js
@@ -1,3 +1,19 @@
-const $search = document.querySelector("[type=search]");
+const $inputSearch = document.querySelector(".input-search");
+const $open = $inputSearch.querySelector(".-open");
+const $close = $inputSearch.querySelector(".-close");
 
-$search.addEventListener('click', () => $search.classList.toggle('-active'));
+$open.addEventListener('click', event => {
+  event.preventDefault(); 
+
+  $inputSearch.classList.toggle("-active");
+  $open.classList.remove("-active");
+  $close.classList.add("-active");
+})
+
+$close.addEventListener('click', event => {
+  event.preventDefault();
+
+  $inputSearch.classList.toggle("-active");
+  $close.classList.remove("-active");
+  $open.classList.add("-active");
+})
